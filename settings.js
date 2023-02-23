ee11c1d3fa5d913b5c4457d31ee49a583d36e2f2
Riswanganz <100130164+Riswanganz@users.noreply.github.com>
2023-02-23 15:27:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'Darkness'// yourname
 global.MONGO_DB_URI = "mongodb+srv://mongodbriswan:123riswan123@cluster0.icfofko.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "bandarrandom@gmail.com" //email
-global.email_password = "" //application password email
+global.your_email = "riswangans03@gmail.com" //email
+global.email_password = "ettpxosutktfxtqn" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
